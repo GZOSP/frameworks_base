e71d3ea7db69408b90c6e1ba85b615fc062c25bc
cphelps76 <cphelps76@droidconcepts.com>
2014-03-19 16:23:35 +0000

    base: Add back battery bar [1/2]
    {x} Adds Battery bar with following features:
    {x} aligment
    {x} thickness
    {x} charging animation
    
    @mydongistiny: updated for oreo
    Change-Id: Ie9a168121dc569ec3af068a4bda6b46a4e896b2d
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4343,6 +4343,32 @@ public final class Settings {
         public static final String ANSWER_VOLUME_BUTTON_BEHAVIOR_ANSWER = "call_volume_answer";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String BATTERY_BAR_LOCATION = "battery_bar_location";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_COLOR = "battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_THICKNESS = "battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_STYLE = "battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String BATTERY_BAR_ANIMATE = "battery_bar_animate";
+
+        /**
          * @hide
          */
         public static final String STATUS_BAR_LOGO = "status_bar_logo";

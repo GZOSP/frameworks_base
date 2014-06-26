2adeee51261190c5ea81cafc85cddff4b2be8120
Danesh M <daneshm90@gmail.com>
2014-06-25 21:31:49 -0700

    PowerManager: Add proximity check on wake
    This consists of the following patches:
    
    ProximityWake : Add support for checking proximity when waking device
    
    Change-Id: Ia5ff446d88435773acccc7467e66009000dc20cb
    
    Be sure to shutdown the proximity sensor after display wake
    
    This fixes:
    http://review.cyanogenmod.org/#/c/66657/
    
    Change-Id: I1768f13cb48c3e056d0e137642690fc3d650c4ab
    
    PowerManager : Make proximity check opt-in
    
    Change-Id: I7cf64a82530da2e655018e43683ba7a5584d54f8
    
    Framework : Fix proximity wakeup reference
    
    Change-Id: Ia40ca6fbf6e37fbb3996f2384f725c30b88c2131
    
    [1/2] Frameworks: let InCallUI handle proximity sensor for incoming calls
    
    Fixes "while 'prevent accidental wake-up' is on, proximity sensor
    doesn't control screen state on incoming call. Must manually
    turn on screen to see who's calling."
    
    Change-Id: I738b3bbd328992c8a8010a0c8d9aa1e444cfa9f9
    
    power: Fix locking for wakeup proximity check
    
     * Often I'm noticing high power consumption due to the proximity sensor
       never turning off. Add synchronization around this code to ensure
       ordering.
    
    Change-Id: I8ccf3152166cd896e0ce2551c01b8ac8f501d782
    
    add config for proximity check on screen on default value
    
    Change-Id: I7e6ae08e23ee147a37986aa6aa9b3176b301a09e
    
    Services : Ensure proximity check is taken into consideration
    
    Only do the proximity check if explicitly being asked to do so.
    This is a patch that was never ported from cm-11.0 :
    2293e60c7de011782e59486605c04b9f67a1e24f
    
    issue-id: CYNGNOS-1223
    
    Change-Id: Id1be619e638de6a45252d8390eab6e38bd5de5a7
    
    services: button brightness should check proximity
    
    Change-Id: I10a72fa6d8b520e86cfef3d7ded489fd7fa936d2
    
    powermanager: Initialize SensorManager in systemReady()
    
     * Service startup ordering is different on M and the sensor service
       is not ready during construction of the PowerManager. This was
       causing a hang on boot. Move it to systemReady instead.
    
    Change-Id: I9e0293484e1aaaf9f82f9c75680bec558607c54d
    
    fw: Move proximitycheck to CMSettings.
    
    Change-Id: Ie80166c990363d3846925b189a1f7001a7fd56f8
    
    PowerManagerService: Log when proximity blocks wake
    
    There is currently no indication when the proximity sensor
    is allowed to prevent the system from waking up.  This can
    make it hard to debug weird failed wakes.  Add a log to
    facilitate debugging wakup problems.
    
    Change-Id: I7df34e13b1268a4e7211fb06ccd88f2053c0af0a
    (cherry picked from commit 21ddba548589fa38907a45789f893c7a08040875)
    
    base: Fix proximity check on power key
    
    To test, enable "Settings >> Display >> Prevent accidental wakeup",
    hold your hand over the proximity sensor and turn your phone on.
    
    Prior to this change, it will wake up the phone and after the change
    it will not.
    
    Change-Id: Ied300108cebb31dedf228e85510abebb3e8a4152
    
    base: Fix proximity check on non power key
    
    To test, enable "Settings >> Display >> Prevent accidental wake-up"
    and "Settings >> Buttons >> Home button Wake up device", hold your
    hand over the proximity sensor and turn your phone on by home key.
    
    Prior to this change, it will wake up the phone and after the change
    it will not.
    
    Change-Id: Ifd14ff2ad9cd2cbc64209b9cf19e3c0ee0b6f40f
    
    only use proximity checks on wakeup keys
    
    The power button should always wake the screen regardless of the
    proximity; only check proximity when the wake up key code is WAKEUP.
    
    Ticket: CYNGNOS-2579
    
    Change-Id: I686ad22d513a3d939be3a0a5c3b752339b2383d3
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Disable proximity check on power key properly
    
    Change-Id: If1ca0fa02805ce2fbe5a9ac1ab56a5e62beca4e1
    
    The change-id below is so that Gerrit won't be confused.
    Change-Id: Ia5ff446d88435773acccc7467e66009000dc20cb
    
    Change-Id: Ib61b19dd5c945fa6605260c31c4d0f21836d4541

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3793,7 +3793,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 && mGlobalKeyManager.shouldHandleGlobalKey(keyCode, event)) {
             if (isWakeKey) {
                 wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey,
-                        PowerManager.WAKE_REASON_WAKE_KEY, "android.policy:KEY");
+                        PowerManager.WAKE_REASON_WAKE_KEY, "android.policy:KEY", true);
             }
             return result;
         }
@@ -4148,7 +4148,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         if (isWakeKey) {
             wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey,
-                    PowerManager.WAKE_REASON_WAKE_KEY, "android.policy:KEY");
+                    PowerManager.WAKE_REASON_WAKE_KEY, "android.policy:KEY",
+                    event.getKeyCode() == KeyEvent.KEYCODE_WAKEUP); // Check prox only on wake key
         }
 
         return result;
@@ -4595,6 +4596,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private boolean wakeUp(long wakeTime, boolean wakeInTheaterMode, @WakeReason int reason,
             String details) {
+        return wakeUp(wakeTime, wakeInTheaterMode, reason, details, false);
+    }
+
+    private boolean wakeUp(long wakeTime, boolean wakeInTheaterMode, @WakeReason int reason,
+            String details, final boolean withProximityCheck) {
         final boolean theaterModeEnabled = isTheaterModeEnabled();
         if (!wakeInTheaterMode && theaterModeEnabled) {
             return false;
@@ -4605,7 +4611,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     Settings.Global.THEATER_MODE_ON, 0);
         }
 
-        mPowerManager.wakeUp(wakeTime, reason, details);
+        if (withProximityCheck) {
+            mPowerManager.wakeUpWithProximityCheck(wakeTime, reason, details);
+        } else {
+            mPowerManager.wakeUp(wakeTime, reason, details);
+        }
         return true;
     }
 

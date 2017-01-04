6197042ba2ed54695300c111b7acb24b10c18587
Jon Haus <haus.xda@gmail.com>
2017-01-05 00:20:06 +0100

    fw_base: Status network traffic [1/2]
    -----
    abc ezio84:
    use O darkintensity api
    use arrows vectors from aosp net arrows commit (thanks @Mazda-- for this find!): https://github.com/ezio84/abc_frameworks_base/commit/ad71ae1fd09f9a7392ab2eba7b766c8b088319b4
    fix visual glitch on statusbar expand (arrows showing and quickly hiding again)
    don't show arrows when no data
    remove unit settings (KB is good)
    remove interval (set to 1sec by def) and single arrow options
    don't get settings values again when not needed
    fix padding between arrows and speed values and expose the padding value
    code cleanup
    -----
    
    Change-Id: If32bcd7764fddf00bc926b3bf6f908518b708f70
    
    Add padding to network traffic arrows
    
    Change-Id: Ia59f1b9ccec415f4452f1f7f987a9393bedce587
    
    Fix janky traffic indicator tint
    
    This is best explained by looking at the jank
    
    https://i.imgur.com/EajdLei.png
    
    Change-Id: I8d4df1aab769755a939dcba6571cb5f0e09b243d
    
    Network traffic autohide threshold: default value 1 as in AE
    
    Change-Id: Id738797d02d7c7e89790a2ca3b70c9be2f78f5fa

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4093,6 +4093,18 @@ public final class Settings {
         private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -4278,6 +4290,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(EGG_MODE);
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
             PRIVATE_SETTINGS.add(DISPLAY_COLOR_MODE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
         }
 
         /**

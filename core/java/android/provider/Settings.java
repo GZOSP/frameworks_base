4713ab3bbb7bfbaf202a7ba5d09d2486c200498c
Ivan Iskandar <iiiiskandar14@gmail.com>
2017-07-28 04:56:45 +0200

    base: Introduce SubstratumService
    This change deprecates ThemeInterfacer, so for testers, please do not
    compile ThemeInterfacer with this change!
    
    Change-Id: I8c9f331ca7e4c68c35ea13bed587775ee4abeb30
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8133,6 +8133,13 @@ public final class Settings {
         public static final String QQS_COUNT = "sysui_qqs_count";
 
         /**
+         * Force authorize Substratum (or equivalent) frontend calling packages by ThemeInterfacer
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear

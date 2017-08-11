ebb8209af013a5fb4c4f7484c0dc68262030abae
siankatabg <siankata91@gmail.com>
2017-08-11 14:28:55 +0100

    base: Add oneplus camera support
    Change-Id: I65a393e8c1892d01f767713273a0d904fc7d2140

--- core/java/android/hardware/camera2/CameraCharacteristics.java
+++ core/java/android/hardware/camera2/CameraCharacteristics.java
@@ -26,6 +26,7 @@ import android.util.Rational;
 
 import java.util.Collections;
 import java.util.List;
+import android.app.ActivityThread;
 
 /**
  * <p>The properties describing a
@@ -204,6 +205,12 @@ public final class CameraCharacteristics extends CameraMetadata<CameraCharacteri
      */
     @Nullable
     public <T> T get(Key<T> key) {
+        if(key == INFO_SUPPORTED_HARDWARE_LEVEL){
+            String packageName = ActivityThread.currentOpPackageName();
+            if(packageName.equals("com.oneplus.camera")){
+                return (T)new Integer(0x2);
+            }
+        }
         return mProperties.get(key);
     }
 

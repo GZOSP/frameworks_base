d392120ab8cb40eb586061fa8e8416c7847f8e48
ezio84 <brabus84@gmail.com>
2017-09-03 15:31:56 +0200

    Add api to toggle flashlight and check if device has flashlight
    statusbar service getter borrowed from PhoneWindowManager code
    
    use PackageManager to check if the device has flashlight
    
    use aosp flashcontroller to handle the flash toggle like the flashlight
    tile does
    
    Change-Id: I4e8fe3351b408aa55573b191baa1bc742962a85c

--- core/java/com/android/internal/util/validus/ValidusUtils.java
+++ core/java/com/android/internal/util/validus/ValidusUtils.java
@@ -17,8 +17,13 @@
 package com.android.internal.util.validus;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.PowerManager;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.SystemClock;
+
+import com.android.internal.statusbar.IStatusBarService;
 
 /**
  * Some custom utilities
@@ -29,6 +34,38 @@ public class ValidusUtils {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
         if (pm!= null) {
             pm.goToSleep(SystemClock.uptimeMillis());
+        }
+    }
+
+    public static boolean deviceHasFlashlight(Context ctx) {
+        return ctx.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+
+    public static void toggleCameraFlash() {
+        FireActions.toggleCameraFlash();
+    }
+
+    private static final class FireActions {
+        private static IStatusBarService mStatusBarService = null;
+        private static IStatusBarService getStatusBarService() {
+            synchronized (FireActions.class) {
+                if (mStatusBarService == null) {
+                    mStatusBarService = IStatusBarService.Stub.asInterface(
+                            ServiceManager.getService("statusbar"));
+                }
+                return mStatusBarService;
+            }
+        }
+
+        public static void toggleCameraFlash() {
+            IStatusBarService service = getStatusBarService();
+            if (service != null) {
+                try {
+                    service.toggleCameraFlash();
+                } catch (RemoteException e) {
+                    // do nothing.
+                }
+            }
         }
     }
 }

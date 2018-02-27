182c375b27cdf81f87e83378d0dbfddce57a5ca4
psyafter <psyafter@gmail.com>
2018-02-27 09:57:11 +0200

    Revert "fw/base: allow home button to wake device [1/2]"
    This reverts commit e85e5d323831e65895886e03c4865477fe0ca0ea.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4371,12 +4371,6 @@ public final class Settings {
         public static final String POWERMENU_SCREENRECORD = "powermenu_screenrecord";
 
         /**
-         * Whether to wake the screen with the home key, the value is boolean.
-         * @hide
-         */
-        public static final String HOME_WAKE_SCREEN = "home_wake_screen";
-
-        /**
          * Whether to use gesture anywhere feature.
          * @hide
          */

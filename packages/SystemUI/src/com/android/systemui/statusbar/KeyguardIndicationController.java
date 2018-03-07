c3e3df791133a7234776101f02c1c96d02c5064b
ezio84 <brabus84@gmail.com>
2018-03-08 00:42:23 +0100

    Ambient: show battery level on bottom
    and hide stock charging indicator near the clock,
    use a tilde in the level textview
    
    Change-Id: I58a284c5feffc3e3881a92d32f416cc706f874cf

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -52,6 +52,8 @@ import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.util.wakelock.SettableWakeLock;
 import com.android.systemui.util.wakelock.WakeLock;
 
+import java.text.NumberFormat;
+
 /**
  * Controls the indications and error messages shown on the Keyguard
  */
@@ -91,6 +93,7 @@ public class KeyguardIndicationController {
     private int mChargingWattage;
     private int mTemperature;
     private String mMessageToShowOnScreenOn;
+    private int mLevel;
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
@@ -295,7 +298,9 @@ public class KeyguardIndicationController {
                     mTextView.setTextColor(Color.WHITE);
                     mTextView.switchIndication(mTransientIndication);
                 } else {
-                    mTextView.switchIndication(null);
+                    CharSequence chargeIndicator = (mPowerPluggedIn ? "~" : "") +
+                            NumberFormat.getPercentInstance().format(mLevel / 100f);
+                    mTextView.switchIndication(chargeIndicator);
                 }
                 return;
             }
@@ -451,6 +456,7 @@ public class KeyguardIndicationController {
             mChargingWattage = status.maxChargingWattage;
             mTemperature = status.temperature;
             mChargingSpeed = status.getChargingSpeed(mSlowThreshold, mFastThreshold);
+            mLevel  = status.level;
             updateIndication();
             if (mDozing) {
                 if (!wasPluggedIn && mPowerPluggedIn) {

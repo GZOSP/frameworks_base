1d0d09f6bd31f3e4ef2f0dc62781aa902b8a274c
Prema Chand Alugu <premaca@gmail.com>
2018-04-16 09:12:00 +0530

    Revert "base: Introduce SubstratumService"
    This reverts commit 4713ab3bbb7bfbaf202a7ba5d09d2486c200498c.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8350,13 +8350,6 @@ public final class Settings {
         public static final String QQS_COUNT = "sysui_qqs_count";
 
         /**
-         * Force authorize Substratum (or equivalent) frontend calling packages by ThemeInterfacer
-         * The value is boolean (1 or 0).
-         * @hide
-         */
-        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
-
-        /**
          * Enable face auto unlock on secure lock screens
          *
          * @hide
--- core/java/com/android/internal/substratum/ISubstratumHelperService.aidl
+++
@@ -1,7 +0,0 @@
-package com.android.internal.substratum;
-
-oneway interface ISubstratumHelperService {
-    void applyBootAnimation();
-    void applyShutdownAnimation();
-    void applyProfile(in String name);
-}

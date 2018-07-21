5a6a4689fe0232a153cde3ad626b5ced4731c26d
Vachounet <vachounet@live.fr>
2018-07-21 21:21:37 +0200

    [1/2] base: implement OP like navigation gestures
    Change-Id: I68453353d9d5d6321c1a385744e5b90e2e1a413e
    
    base: add long press home handling to nav gestures
    
    insread of fixed recents use the long press home setting
    
    Change-Id: I94ef0dcb5180c1b305438acf445767fa50f6b670
    
    base: cleanup nav gestures code
    
    Change-Id: I6d412e58e2484cb84307437f67577a5c584c913b
    
    base: cleanup and fix a few nav gestures issues
    
    make sure we dont do anything while on keyguard
    long press should only be active in home location
    make threshold values resource values
    make sure if gestures are disabled no edge events are lost
    
    Change-Id: Ifb36c9b0c868257e73988f09dc11c991a0b3b9bb

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -7623,4 +7623,14 @@ public class WindowManagerService extends IWindowManager.Stub
             }
         }
     }
+
+    @Override
+    public boolean isGestureButtonEnabled() {
+        return this.mPolicy.isGestureButtonEnabled();
+    }
+
+    @Override
+    public boolean isGestureButtonRegion(int i, int i2) {
+        return this.mPolicy.isGestureButtonRegion(i, i2);
+    }
 }

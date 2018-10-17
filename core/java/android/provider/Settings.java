b78f4c4d991f93e45a2cf55fa7cf54271773ca70
calebcabob <calphonic@gmail.com>
2018-10-16 22:16:44 -0700

    add black theeeeeme
    Change-Id: I1f12aebe647fe0fd58c0f45d8b54290930e0b5d8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4296,6 +4296,7 @@ public final class Settings {
          * 0 - wallpaper based
          * 1 - force light
          * 2 - force dark
+         * 3 - force black
          * @hide
          */
         public static final String SYSTEM_THEME = "system_theme";
